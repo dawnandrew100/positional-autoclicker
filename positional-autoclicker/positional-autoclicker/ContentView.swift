c197ed49a651fe834bf6d164d2fae3e9856fabfb
Andrew Hennis <81731313+dawnandrew100@users.noreply.github.com>
2024-04-14 18:31:09 -0400

    Add mac app support via xcode
    App boilerplate and code generated using xcode in swift langauge

---
+++ positional-autoclicker/positional-autoclicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  positional-autoclicker
+//
+//  Created by Andrew Hennis on 4/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ positional-autoclicker/positional-autoclicker/positional_autoclickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  positional_autoclickerApp.swift
+//  positional-autoclicker
+//
+//  Created by Andrew Hennis on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct positional_autoclickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
